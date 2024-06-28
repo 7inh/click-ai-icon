71f5a0bf81ecb7e20ebfc161fe16cc90b9379bc0
7inh <tranquoclinh@gmail.com>
2024-06-29 00:14:45 +0700

    chore: Remove Createfile.ipynb icon component

--- src/icons/Createfile.ipynb.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import IconSvg from './Createfile.ipynb.svg';
-
-
-const Createfile.ipynb: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <IconSvg {...props} />
-);
-
-export default Createfile.ipynb;
